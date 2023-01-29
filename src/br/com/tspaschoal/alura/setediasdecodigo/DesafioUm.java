1d9b1580b444ae7f2c158a1565846a4fe2325bcb
thiagosp <o.tpaschoal@gmail.com>
2023-01-29 09:39:50 -0300

    feat: adicionado DesafioUm

--- src/br/com/tspaschoal/alura/setediasdecodigo/DesafioUm.java
+++ src/br/com/tspaschoal/alura/setediasdecodigo/DesafioUm.java
@@ -9,24 +9,35 @@ import java.net.http.HttpResponse;
 
 public class DesafioUm {
 
-    private static final String ENDPOINT = "https://imdb-api.com/en/API/Top250Movies/";
+    private static final String BASE_URL = "https://imdb-api.com";
 
     private static final String API_TOKEN = "k_8rt57f5x";
 
-    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
-
-        final var uri = new URI(String.format("%s%s", ENDPOINT, API_TOKEN));
-
-        final var request = HttpRequest.newBuilder()
-                .GET()
-                .header("Content-Type", "application/json")
-                .header("Accept", "application/json")
-                .uri(uri)
-                .build();
-
-        final var response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString()).body();
-
+    public static void main(String[] args) {
+        final var response = getImdbApiResponse();
         System.out.println(response);
+    }
 
+    private static String getImdbApiResponse() {
+        try {
+            final var request = getHttpRequest();
+            return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString()).body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("erro ao processar requisição", e);
+        }
+    }
+
+    private static HttpRequest getHttpRequest() {
+        try {
+            final var uri = new URI(String.format("%s/en/API/Top250Movies/%s", BASE_URL, API_TOKEN));
+            return HttpRequest.newBuilder()
+                    .GET()
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .uri(uri)
+                    .build();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("erro ao criar uri", e);
+        }
     }
 }
